40dd5ea2898a8723a41b0c3a098d0c6383a9a161
Ramesh Marikhu <marikhu@gmail.com>
2020-08-03 12:40:52 +0700

    Added inference.sh and a test image from Rama4. Results from YOLOv4 seems exactly the same as from YOLOv3

---
+++ inference.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+./darknet detector test VehicleDetect-Yolo-Rama4-Set2.data VehicleDetect-Yolo-Rama4-Set2.cfg models_VehicleDetect-Yolo-Rama4-Set2/VehicleDetect-Yolo-Rama4-Set2_114002.weights
+
+# NOTE: it will ask the image name after model is loaded, and it will display the detections on console, and it will try to display the image with detections if display is supported.
+# Results are saved to predictions.jpg
