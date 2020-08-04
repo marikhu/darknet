144c1006bf18317803a658e1fe8c1bac0f61c57a
Ramesh Marikhu <marikhu@gmail.com>
2020-08-04 10:47:03 +0700

    Updated scripts; Trained YOLOv4 with YOLOv4 configuration, and computed mAP for obtained models, but well, using the *_best.weights file

--- compute-mAP.sh
+++ compute-mAP.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 
-./darknet detector map VehicleDetect-Yolo-R
-ama4-Set2.data VehicleDetect-Yolo-Rama4-Set2.cfg models_VehicleDetect-Yolo-Rama4-Set2/VehicleDetect-Yolo-Rama4-Set2_114002.weights
-
+#./darknet detector map VehicleDetect-Yolo-Rama4-Set2.data VehicleDetect-Yolo-Rama4-Set2.cfg models_VehicleDetect-Yolo-Rama4-Set2/VehicleDetect-Yolo-Rama4-Set2_114002.weights
+./darknet detector map VehicleDetect-Yolo-Rama4-Set2.data VehicleDetect-Yolo-Rama4-Set2.cfg models_VehicleDetect-Yolo-Rama4-Set2/VehicleDetect-Yolo-Rama4-Set2_best.weights
--- visualization/visualization_iou.py
+++ visualization/visualization_iou.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #%matplotlib inline
 
 lines = 5000  
-result = pd.read_csv('log_iou.txt', skiprows=[x for x in range(lines) if (x%10==0 or x%10==9) ] ,error_bad_lines=False, names=['Region Avg IOU', 'Class', 'Obj', 'No Obj', 'Avg Recall','count'])
+result = pd.read_csv('log_iou-2.txt', skiprows=[x for x in range(lines) if (x%10==0 or x%10==9) ] ,error_bad_lines=False, names=['Region Avg IOU', 'Class', 'Obj', 'No Obj', 'Avg Recall','count'])
 result.head()
 
 result['Region Avg IOU']=result['Region Avg IOU'].str.split(': ').str.get(1)
@@ -42,4 +42,4 @@ ax.legend(loc='best')
 ax.set_title('The Region Avg IOU curves')
 ax.set_xlabel('batches')
 # fig.savefig('Avg IOU')
-fig.savefig('Region Avg IOU')
+fig.savefig('Region-Avg-IOU')
